326421a653e25321a2f2ec8de273f8c9d9622ccd
Vinay DG <vinay.dg@travancoreanalytics.com>
2025-02-04 09:34:33 +0530

    correction of class name

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ for (let i = 0; i < rows; i++) {
   const rowElement = document.createElement("div");
   for (let j = 0; j < columns; j++) {
     const columnElement = document.createElement("div");
-    columnElement.classList.add(".pixel");
+    columnElement.classList.add("pixel");
     rowElement.appendChild(columnElement);
   }
   container.appendChild(rowElement);
