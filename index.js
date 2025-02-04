3c72d4fcc47b643871d8ad78f9d9b305e3b0a5e8
Vinay DG <vinay.dg@travancoreanalytics.com>
2025-02-04 10:51:40 +0530

    refacture canvas creating logic to a function

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
+function creatCanvas(rows, columns) {
+  container.innerHTML = "";
+  for (let i = 0; i < rows; i++) {
+    const rowElement = document.createElement("div");
+    for (let j = 0; j < columns; j++) {
+      const columnElement = document.createElement("div");
+      columnElement.classList.add("pixel");
+      rowElement.appendChild(columnElement);
+    }
+    rowElement.classList.add("row");
+    container.appendChild(rowElement);
+  }
+}
 const container = document.querySelector(".container");
 
 let rows = 16;
 let columns = 16;
-
-for (let i = 0; i < rows; i++) {
-  const rowElement = document.createElement("div");
-  for (let j = 0; j < columns; j++) {
-    const columnElement = document.createElement("div");
-    columnElement.classList.add("pixel");
-    rowElement.appendChild(columnElement);
-  }
-  rowElement.classList.add("row");
-  container.appendChild(rowElement);
-}
+creatCanvas(rows, columns);
